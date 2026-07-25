e5e48c8f12e6c39a176cd369f2ed1c134369a71d
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-07-25 13:51:18 +0200

    Add SalesInvoice Index view

---
+++ GymFlow.Domain/Extensions/InvoiceStatusExtensions.cs
@@ -0,0 +1,26 @@
+﻿using GymFlow.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymFlow.Domain.Extensions
+{
+    public static class InvoiceStatusExtensions
+    {
+        public static string ToBadgeClass(this InvoiceStatus status)
+        {
+            return status switch
+            {
+                InvoiceStatus.Draft => "bg-secondary",
+                InvoiceStatus.Paid => "bg-success",
+                InvoiceStatus.Partial => "bg-warning",
+                InvoiceStatus.Unpaid => "bg-danger",
+                InvoiceStatus.Cancelled => "bg-warning",
+                _ => "bg-secondary"
+            };
+        }
+
+    }
+}
